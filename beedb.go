16b2d1b5b3739fd12d050452f176967543bc6185
Magnus Erixzon <magnus@erixzon.com>
2014-04-09 11:54:33 +0100

    Snake case the primary key name when saving

--- beedb.go
+++ beedb.go
@@ -388,8 +388,8 @@ func (orm *Model) Save(output interface{}) error {
 	if orm.TableName == "" {
 		orm.TableName = getTableName(output)
 	}
-	id := results[orm.PrimaryKey]
-	delete(results, orm.PrimaryKey)
+	id := results[snakeCasedName(orm.PrimaryKey)]
+	delete(results, snakeCasedName(orm.PrimaryKey))
 
 	if id == nil {
 		return fmt.Errorf("Unable to save because primary key %q was not found in struct", orm.PrimaryKey)
